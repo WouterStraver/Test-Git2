7cda71a2cbd572a131e6684ba2b810395d041146
Wouter Straver <wouterstraver@mbp-van-wouter.lan>
2014-05-02 20:30:02 +0200

    Initial Commit

---
+++ Git test/WSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WSAppDelegate.h
+//  Git test
+//
+//  Created by Wouter Straver on 02-05-14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Git test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git test
+//
+//  Created by Wouter Straver on 02-05-14.
+//  Copyright (c) 2014 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WSAppDelegate class]));
+    }
+}
